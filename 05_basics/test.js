db32964fe592ed48e525efdabd3e2e9de16abb32
PiyushG-dev <pjgpiyush@gmail.com>
2024-02-15 12:39:34 +0530

    numbers and maths 1

---
+++ 05_basics/test.js
@@ -0,0 +1,16 @@
+const num = 400;
+const str = "hey whats up";
+const str2 = new String("hey bro");
+
+// console.log(str2.concat("sup"));
+
+const number = new Number(400);
+
+// console.log(num);
+// console.log(number);
+
+const numMain = 10000000000;
+
+const locale = numMain.toLocaleString("en-IN");
+
+console.log(locale);
